e979930ef21cf6d7f69e6a4c59bdf2d05017cd52
Harshada Jagdish Mundwadkar <harshu@Harshadas-MacBook-Air.local>
2025-06-21 02:11:36 +0530

    optimal code for sorting an array

--- Bubble.java
+++ Bubble.java
@@ -1,19 +1,24 @@
 class Bubble{
     public static void main(String[] args ){
         int arr[]={3,2,5,4,1};
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
+        for(int i=0;i<arr.length-1;i++){
+            int flag=0;
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
                     int temp;
-                    temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    flag=1;
                    
                 }
+            }
+            if(flag==0){
+                break;
             }
         }
         for(int k=0;k<arr.length;k++){
             System.out.println(arr[k]);
         }
     }
-}
+} 
