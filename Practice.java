833b467232a05018bf5b9d10d5c301b46ddfccce
Harshada Jagdish Mundwadkar <harshu@Harshadas-MacBook-Air.local>
2025-06-25 23:12:36 +0530

    practice code

--- Practice.java
+++ Practice.java
@@ -1,12 +1,12 @@
 class Practice{
+    public static void practice(){
+      //contribute your logical code 
+
+    }
+
     public static void main(String[] args){
-        int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        practice();
+       
 
     
  
