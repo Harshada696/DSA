ae3d54f0d038e34b347493ae78146ce56aa0eeb4
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-06-27 00:02:49 +0530

    String programs

---
+++ Strings.java
@@ -0,0 +1,20 @@
+class Strings{
+    public static void print(String name){
+        
+        for(int i=0;i<name.length();i++)
+        {
+            System.out.println(name.charAt(i));
+        }
+    }
+    public static void reverse(String name){
+        for(int i=name.length()-1;i>=0;i--){
+            System.out.print(name.charAt(i));
+        }
+    }
+    public static  void main(String[] args){
+        String name="harshu";
+        
+        reverse(name);
+        
+    }
+}
