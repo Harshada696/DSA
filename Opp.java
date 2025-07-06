68031280a4f9c4369dec5c9a6fe490e7d70f9fda
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-06 21:45:25 +0530

    commited

--- Opp.java
+++ Opp.java
@@ -1,31 +1,45 @@
-
-public class Opp extends Student{
-    public static void main(String [] args){
-        Student sc =new Student(); //creating object of class student
-        sc.name="harshu";//accsesing attributes using object
-        sc.age=19;
-        sc.print();//acessing method of class student 
+// Main class
+public class Opp extends Student {
+    public static void main(String[] args) {
+        // Object of Student
+        Student sc = new Student();
+        sc.name = "harshu";
+        sc.age = 19;
+        sc.print();
         sc.print_myname();
-    
 
-
-
+        // Object of Student_info
+        Student_info st = new Student_info();
+        st.displayInfo();
     }
 }
-  class Student{ // we cannot keep two calsses public in single files 
-    //attributes
+
+// Supporting class
+class Student_info {
+    String name = "harshu";
+    int roll_no = 8042;
+    double marks = 90.60;
+
+    // Method to print student info
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll_no);
+        System.out.println("Marks: " + marks);
+    }
+}
+
+// Another supporting class
+class Student {
     String name;
     int age;
 
-    //method
-    public void print(){
-        System.out.println("name is :"+name);
-        System.out.println("Age is this :"+age);
-    }
-    public void print_myname(){
-        String name1="harshu";
-        System.out.println("my name is "+name1);
+    public void print() {
+        System.out.println("Name is: " + name);
+        System.out.println("Age is: " + age);
     }
 
-
+    public void print_myname() {
+        String name1 = "harshu";
+        System.out.println("My name is: " + name1);
+    }
 }
