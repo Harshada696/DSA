d5c50fbc64d3de2fb718a16d99d62e5800340246
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-06 21:22:55 +0530

    commited

--- Opp.java
+++ Opp.java
@@ -1,13 +1,31 @@
-Class Opp{
-    String name ="name";
-    int num=1;
-    public void add(){
-        System.out.println("name"+name+"num"+num);
+
+public class Opp extends Student{
+    public static void main(String [] args){
+        Student sc =new Student(); //creating object of class student
+        sc.name="harshu";//accsesing attributes using object
+        sc.age=19;
+        sc.print();//acessing method of class student 
+        sc.print_myname();
+    
+
+
+
     }
 }
-Class New extends Oppw{
-    public static void main(){
-        Opp op=new Opp("harshu",10);
-        op.add();
+  class Student{ // we cannot keep two calsses public in single files 
+    //attributes
+    String name;
+    int age;
+
+    //method
+    public void print(){
+        System.out.println("name is :"+name);
+        System.out.println("Age is this :"+age);
     }
+    public void print_myname(){
+        String name1="harshu";
+        System.out.println("my name is "+name1);
+    }
+
+
 }
