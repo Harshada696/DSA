6b2526bc4702f7d098319c81f90ef745b86c5865
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-08 21:03:51 +0530

    final commit

--- Simple.java
+++ Simple.java
@@ -1,5 +1,14 @@
 class Simple{
+    public static void print_table(){
+        int a=2;
+        
+        for(int i=1;i<=10;i++){
+            System.out.println("2 *"+i+"="+a*i);
+        }
+
+    }
     public static void main(String [] args){
+        print_table();
         int arr[]={7,6,5,4,3,2,1};
         int search=4;
         int flag=0;
