d4c42366fb05bc4364f31f437788e7210ba4e06d
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-08 22:36:11 +0530

    commit

--- Max.java
+++ Max.java
@@ -1,12 +1,16 @@
 class Max{
     public static void main(String[]args ){
-        String name="harshada";
-        String reverse="";
-        for(int i=name.length()-1;i>=0;i--){
-             reverse+=name.charAt(i);
+    int arr[]={1,2,2,3,2};
+    for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr.length;j++){
+            int count=0;
+            if(arr[i]==arr[j]){
+                count++
+                System.out.print("duplicates are :"+arr[i]);
+            }
+        }
 
-         }
-         System.out.print(reverse);
-     
-    }
+  }
+    
+}
 }
