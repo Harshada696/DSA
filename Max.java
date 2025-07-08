b07b89caa33edeec7be47f69563538282a8620cc
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-08 21:41:28 +0530

    final commit

--- Max.java
+++ Max.java
@@ -1,12 +1,8 @@
 class Max{
-    public static void main(String [] args){
-       int arr[]={1,2,3,4,5,6};
-       int sum=0;
-       for(int i=0;i<arr.length;i++){
-        sum+=arr[i];
-
-       }
-       System.out.println("Sum of arr elements is :"+sum);
-
+    public static void main(String[]args ){
+        char arr[]={'h','a','r','s','h','a','d','a'};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
