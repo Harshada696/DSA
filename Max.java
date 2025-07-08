37235fc5eab80b76e2614a306f96c494593d3496
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-08 23:10:53 +0530

    final commit

--- Max.java
+++ Max.java
@@ -1,16 +1,23 @@
-class Max{
-    public static void main(String[]args ){
-    int arr[]={1,2,2,3,2};
-    for(int i=0;i<arr.length;i++){
-        for(int j=0;j<arr.length;j++){
-            int count=0;
-            if(arr[i]==arr[j]){
-                count++
-                System.out.print("duplicates are :"+arr[i]);
+//Most frequent element
+class Max {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 2, 3, 2};
+        int maxCount = 0;
+        int mostFrequent = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                mostFrequent = arr[i];
             }
         }
 
-  }
-    
-}
+        System.out.println("Most frequent element: " + mostFrequent + " (appears " + maxCount + " times)");
+    }
 }
