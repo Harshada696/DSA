8a4ca4dfe32da8b7cbcb571f01efdabdc70e3591
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-11 14:33:44 +0530

    commited

--- Number_sum.java
+++ Number_sum.java
@@ -19,5 +19,21 @@ class Number_sum{
             a=a/10;
         }
         System.out.println("sum of the number :"+sum);
+
+        // check if a number is palindrome
+        int rev_number=111;
+        int rev1=0;
+        int original=rev_number;
+        while(rev_number !=0){
+            int digit=rev_number%10;
+            rev1=rev1*10+digit;
+            rev_number/=10;
+        }
+        if(rev1 == original){
+            System.out.println("number is palindrome");
+        }
+        else{
+            System.out.println("number is not palindrome");
+        }
         
 }}
