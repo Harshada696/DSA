aa415d3e859c859abff0fba7a07c1c71d7d110ef
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-11 14:30:30 +0530

    commited

--- Number_sum.java
+++ Number_sum.java
@@ -1,5 +1,16 @@
 class Number_sum{
     public static void main(String []args){
+        //reverse a number 
+        int rev_a=123;
+        int rev=0;
+        while(rev_a !=0){
+            int digit=rev_a%10;
+            rev=rev*10+digit;
+            rev_a/=10;
+
+        }
+        System.out.println("reverse of the number :"+ rev);
+        //sum of digit 
         int a=123;
         int sum=0;
         while(a !=0){
