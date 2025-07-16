83862ab5259bed39a0e905265c84754427def1f0
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-16 20:53:33 +0530

    commited

--- Practice_2.java
+++ Practice_2.java
@@ -1,18 +1,12 @@
 class Practice_2{
 
     public static void main(String args[]){
-          int arr[]={1,2,2,2,2,4};
-          int count=0;
-          for(int i=0;i<arr.length-1;i++){
-            if(arr[i]==arr[i+1]){
-                count++;
-                
-            }
-          }
-          if(count >0){
-            System.out.println("duplicate finds:"+count);
+       int number=3;
+       int sum=0;
+       for(int i=0;i<number;i++){
+        sum+=number;
 
-          }
-          
+       }
+       System.out.println("sum"+sum);
     }
 }
