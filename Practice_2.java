431ef3f3354180c849ff6a3070a34e4a8e709b86
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-17 14:26:48 +0530

    comited

--- Practice_2.java
+++ Practice_2.java
@@ -1,12 +1,26 @@
 class Practice_2{
+        public static void febo(){
+            int n=5;
+            int num1=0;
+            int num2=1;
+            int num3;
+            System.out.println(num1);
+            System.out.println(num2);
+            
+            for(int i=2;i<n;i++){
+                 num3=num1+num2;
+                 System.out.println("febo series :"+ num3);
+                num1=num2;
+                num2=num3;
+
+
+            }
+            
+        }
 
     public static void main(String args[]){
-       int number=3;
-       int sum=0;
-       for(int i=0;i<number;i++){
-        sum+=number;
+        febo();
 
-       }
-       System.out.println("sum"+sum);
+       
     }
 }
