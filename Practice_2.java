08303c4961c6cbbef717cb4981c322bd3583aad6
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-17 22:32:36 +0530

    c

--- Practice_2.java
+++ Practice_2.java
@@ -1,15 +1,20 @@
-class Practice_2{
-        public static void febo(){
-              String name="Harsahda";
-              for(int i=0;i<name.length();i++){
-                System.out.println(name.charAt(i));
-              }
-            
+class Practice_2 {
+    public static void print() {
+        int n = 6;
+        int m = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n  || j == 1|| j == m ) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   "); // print space inside the box
+                }
+            }
+            System.out.println();
         }
+    }
 
-    public static void main(String args[]){
-        febo();
-
-       
+    public static void main(String args[]) {
+        print();
     }
 }
