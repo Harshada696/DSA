416f662e326baad870812497e27e5db1815b9159
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-17 19:30:00 +0530

    finish

--- Practice_2.java
+++ Practice_2.java
@@ -1,20 +1,9 @@
 class Practice_2{
         public static void febo(){
-            int n=5;
-            int num1=0;
-            int num2=1;
-            int num3;
-            System.out.println(num1);
-            System.out.println(num2);
-            
-            for(int i=2;i<n;i++){
-                 num3=num1+num2;
-                 System.out.println("febo series :"+ num3);
-                num1=num2;
-                num2=num3;
-
-
-            }
+              String name="Harsahda";
+              for(int i=0;i<name.length();i++){
+                System.out.println(name.charAt(i));
+              }
             
         }
 
