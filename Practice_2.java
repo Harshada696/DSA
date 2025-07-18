812ebb8cf1f7c71b01fe46b863a9e922323be6f1
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-19 00:11:36 +0530

    new

--- Practice_2.java
+++ Practice_2.java
@@ -4,15 +4,22 @@ class Practice_2{
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         int input;
+        int count=0;
         
         int pass=1234;
        do{
             System.out.println("Enter password:");
              input=sc.nextInt();
+             count++;
+              if(count==3){
+                System.out.println("no more chances remain");
+              }
+              else{
+                System.out.println("Try Again");
+              }
+            
 
-            System.out.println("Try Again");
-
-        }while(pass !=input);
+        }while(count !=3);
 
     
 
---
+++ TOP_10_BY_COMPANY.java
@@ -0,0 +1,118 @@
+class TOP_10_BY_COMPANY{
+       public static void reverse(String str){
+        String reverse="";
+                  for(int i=str.length()-1;i>=0;i--){
+                    reverse+=str.charAt(i);
+
+                  }
+                  System.out.println("Reverse strign is :"+ reverse);
+
+       }
+       
+       public static boolean isPrime(int  prime){
+        if(prime<=1){
+            return false;
+}   else{    for(int i=2;i<=Math.sqrt(prime);i++){
+    if(prime%i==0){
+        return false;
+       
+    }
+  
+}
+return true;
+       }}
+     public static void fact_fun(int fact){
+        int total=1;
+        for(int i=1;i<=fact;i++){
+            total*=i;
+        }
+        System.out.println("factorial of :"+ fact +"is"+total);
+
+     }
+     //fibo series;
+     public static void fibo(){
+        int input=7;
+        int first_num=0;
+        int sec_num=1;
+        System.out.print(first_num);
+        System.out.println(sec_num);
+        for(int i=2;i<input;i++){
+            int third=first_num+sec_num;
+            System.out.print(third);
+             first_num=sec_num;
+            sec_num=third;
+        }
+
+
+     }
+     //palindrome
+     public static void pal(){
+        String str="harshu";
+        String original=str;
+
+        String reverse="";
+                  for(int i=str.length()-1;i>=0;i--){
+                    reverse+=str.charAt(i);
+
+                  }
+                  System.out.println("Reverse strign is :"+ reverse);
+                  if(original.equals(reverse)){
+                    System.out.println("string is palindrome");
+                  }
+                  else{
+                    System.out.println("string is not palindrome");
+                  }
+
+     }
+     //largest number of array
+     public static void max(){
+        int arr[]={1,2,3,4,5};
+        int max=arr[0];
+        
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[0]<arr[i]){
+                max=arr[i];
+            }
+        }
+        System.out.println("max is :"+max);
+
+     }
+     //count the frequency;
+     public static void fre(){
+        int arr[]={1,2,2,4,4,3};
+        int count=0;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                count++;
+                System.out.print("count :"+ arr[i] + count );
+
+            }
+        }
+     }
+     //armstrong;
+     //sort arry;
+     //find second largest;
+
+        public static void main(String []args){
+            int fact=5;
+            int prime=31;
+            String str="harshu";
+            reverse(str);
+           
+           if(isPrime(prime)){
+            System.out.println("its prime ");
+           }
+           else{
+            System.out.println("not primr");
+           }
+
+           fact_fun(fact);
+           fibo();
+           pal();
+           max();
+           fre();
+
+
+        }
+}
