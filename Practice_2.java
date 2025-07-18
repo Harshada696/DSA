85cf0bec8c18cf51ddf6e645b986dbe3bb64b6b7
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-18 22:31:42 +0530

    final git

--- Practice_2.java
+++ Practice_2.java
@@ -1,20 +1,31 @@
-class Practice_2 {
-    public static void print() {
-        int n = 6;
-        int m = 6;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 || i == n  || j == 1|| j == m ) {
-                    System.out.print(" * ");
-                } else {
-                    System.out.print("   "); // print space inside the box
-                }
+class Practice_2{
+    public static void print(){
+        int n=6;
+        for(int i1=n-1;i1>0;i1--){
+            for(int i=i1;i<n-1;i++){
+                 System.out.print("*");
+            }
+            for(int j1=0;j1<2*(n-i1);j1++){
+                System.out.print(" ");
+            }
+            for(int j=i1;j<2*(n-i1);j++){
+                 System.out.print(" ");
             }
             System.out.println();
         }
-    }
-
-    public static void main(String args[]) {
+        
+            
+             for(int i1=0;i1<n-1;i1++){
+            for(int i=i1;i<n-1;i++){
+                 System.out.print("*");
+            }
+            for(int j=i1;j<2*(n-i1);j++){
+                 System.out.print(" ");
+            }
+            System.out.println();
+        }
+        }
+    public static void main(String [] args){
         print();
     }
 }
