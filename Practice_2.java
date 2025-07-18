806532bafd7a3cec57f37245ddb992c9748d14ba
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-18 22:50:03 +0530

    commited

--- Practice_2.java
+++ Practice_2.java
@@ -1,31 +1,20 @@
+import java.util.Scanner;
 class Practice_2{
-    public static void print(){
-        int n=6;
-        for(int i1=n-1;i1>0;i1--){
-            for(int i=i1;i<n-1;i++){
-                 System.out.print("*");
-            }
-            for(int j1=0;j1<2*(n-i1);j1++){
-                System.out.print(" ");
-            }
-            for(int j=i1;j<2*(n-i1);j++){
-                 System.out.print(" ");
-            }
-            System.out.println();
-        }
+
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int input;
         
-            
-             for(int i1=0;i1<n-1;i1++){
-            for(int i=i1;i<n-1;i++){
-                 System.out.print("*");
-            }
-            for(int j=i1;j<2*(n-i1);j++){
-                 System.out.print(" ");
-            }
-            System.out.println();
-        }
-        }
-    public static void main(String [] args){
-        print();
+        int pass=1234;
+       do{
+            System.out.println("Enter password:");
+             input=sc.nextInt();
+
+            System.out.println("Try Again");
+
+        }while(pass !=input);
+
+    
+
     }
 }
