246c8ffe47a59a561963d254ba86849e31ab74eb
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-20 17:58:20 +0530

    commited

--- TOP_10_BY_COMPANY.java
+++ TOP_10_BY_COMPANY.java
@@ -92,6 +92,23 @@ return true;
      }
      //armstrong;
      //sort arry;
+     public static void sort(){
+      int arr[]={3,4,5,2,1,1};
+      for(int i=0;i<arr.length-1;i++){
+        for(int j=0;j<arr.length-1-i;j++){
+          if(arr[j]>arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+
+          }
+        }
+      }
+      System.out.print("sorted arry");
+      for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]);
+      }
+     }
      //find second largest;
 
         public static void main(String []args){
@@ -112,6 +129,7 @@ return true;
            pal();
            max();
            fre();
+           sort();
 
 
         }
