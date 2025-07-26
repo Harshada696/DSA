c2938d33c1fdcbeddffcfd2f4d2178d3a4f568dc
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-26 20:11:37 +0530

    commited

---
+++ reverse.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class reverse{
+    public static void main(String args){
+        int 5;
+        for(int i; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+}
