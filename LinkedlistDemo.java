12ee3386295022e2511954dc6db264ea1c0e5930
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-08-04 22:41:44 +0530

    commited

--- LinkedlistDemo.java
+++ LinkedlistDemo.java
@@ -14,18 +14,30 @@ class LinkedListDemo{
             list.addFirst(0);
             System.out.println("add to first:"+list);*/
             LinkedList <Integer> list=new LinkedList<>();
-            list.add(1);
+            list.add(1);//add element
             list.add(2);
-            list.addFirst(0);
-            list.addLast(3);
+            list.addFirst(0);//add eleemnt to first 
+            list.addLast(3);//add element to last
            /* System.out.println("get element at 3 :"+list.get(3));
             System.out.println("list elements :"+list);
             
             System.out.println("list elements :"+list);
             System.out.println("get element at 3 :"+list.get(3));*/
-            System.out.println("list elements :"+list.removeFirst());
-             System.out.println("list elements :"+list.get(2));//here we underatand that indexing of linked list start from 0
+            System.out.println("list elements :"+list.removeFirst());//remove first element and return head ex:first element of list 
+             System.out.println("list elements :"+list.get(2));//here we underatand that indexing of linked list start from 0(print element at last)
              System.out.println("list eleemnts :"+list);
+             System.out.println("size of element :"+list.size());
+             for(int i=list.size()-1;i>0;i--){//reverse linkedlist
+                System.out.println("reverse lis elemetn :"+list.get(i));
+             }
+             Collections.reverse(list);//reverse list by using pre-defined method 
+            System.out.println("Reversed list: " + list);
+            //convert to array 
+            Object[] array = list.toArray();
+            System.out.println("Array: " + Arrays.toString(array));
+
+
+
 
 
 
