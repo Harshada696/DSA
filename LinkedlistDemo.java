c076fd9f5243d9ff34faaffbb5e8ea40716fbe86
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-08-04 22:10:27 +0530

    commited

--- LinkedlistDemo.java
+++ LinkedlistDemo.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class LinkedListDemo{
     public static void main(String args[]){
-        //LinkedList <Integer> list=new LinkedList <>();
+       /* //LinkedList <Integer> list=new LinkedList <>();
         LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
         list.add(2);
@@ -12,7 +12,23 @@ class LinkedListDemo{
             list.addFirst(1);
             System.out.println("new list:"+list);
             list.addFirst(0);
-            System.out.println("add to first:"+list);
+            System.out.println("add to first:"+list);*/
+            LinkedList <Integer> list=new LinkedList<>();
+            list.add(1);
+            list.add(2);
+            list.addFirst(0);
+            list.addLast(3);
+           /* System.out.println("get element at 3 :"+list.get(3));
+            System.out.println("list elements :"+list);
+            
+            System.out.println("list elements :"+list);
+            System.out.println("get element at 3 :"+list.get(3));*/
+            System.out.println("list elements :"+list.removeFirst());
+             System.out.println("list elements :"+list.get(2));//here we underatand that indexing of linked list start from 0
+             System.out.println("list eleemnts :"+list);
+
+
+
 
     }
 }
