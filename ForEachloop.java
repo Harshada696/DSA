2b5f1f2e4cb7bf38e99ec3985f77e794bda6dd61
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-08-06 00:52:39 +0530

    commited

---
+++ ForEachloop.java
@@ -0,0 +1,14 @@
+class ForEachloop{
+    public static void main(String [] args){
+        //for interger type
+        int arr[]={2,3,4,5};
+        for(int i:arr){
+            System.out.println(i);
+        }
+        //for string type 
+        String [] name={"harshu","banty","bablu"};
+        for(String j:name){
+            System.out.println(j);
+        }
+    }
+}
