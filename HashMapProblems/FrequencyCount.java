1f6158526b0cd2d8a0b84fd50cb72ac3014911b5
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-08-06 02:04:11 +0530

    commited

--- HashMapProblems/FrequencyCount.java
+++ HashMapProblems/FrequencyCount.java
@@ -1,17 +1,13 @@
 import java.util.*;
 class FrequencyCount{
-    public static void main(String [] args){
-        String name[]={"apple","apple","banana","mango","mango"};
-        HashMap <String ,Integer> map=new HashMap<>();
+    public static void main (String [] args){
+        String name[]={"harshu","harshu","khushi"};
+        HashMap <String ,Integer > map=new HashMap<>();
         for(String i:name){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        for(Map.Entry<String ,Integer> entry:map.entrySet()){
-            System.out.println("key :"+entry.getKey()+" value :"+entry.getValue());
+        for(Map.Entry<String,Integer> entry: map.entrySet()){
+            System.out.println("word: "+entry.getKey()+" count: "+entry.getValue());
         }
-
-
-
-
-            }
+    }
 }
