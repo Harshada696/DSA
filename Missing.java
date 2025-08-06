95a4b7ce39b17d3ffb1c61563e0cc791c2d9e3ee
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-08-06 23:19:52 +0530

    commited

---
+++ Missing.java
@@ -0,0 +1,15 @@
+class Minssing{
+public static void main(String args[]){
+    int arr[]={1,2,3,5,6};
+    int n=arr.length+1;
+    int ex_sum=(n*(n+1)/2);
+    int sum=0;
+    for(int i=0;i<arr.length;i++){
+        sum+=arr[i];
+
+    }
+    int missing =ex_sum-sum;
+    System.out.println("missing number :"+ missing);
+
+}
+}
